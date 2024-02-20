f33d36413b5360eef3257cf7e0155c826f6501ca
kasumi993 <khadijag993@gmail.com>
2024-02-20 12:06:46 +0100

    main header + page config

---
+++ src/containers/mainContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>here we will have the header</div>
+  <div class="main">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainContainer"
+}
+</script>
+
+<style scoped>
+
+</style>
