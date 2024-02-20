95c028d2f10bf5cafb3a2d324223e7a67290f209
kasumi993 <khadijag993@gmail.com>
2024-02-20 14:16:14 +0100

    add header exist test

---
+++ cypress/e2e/header.cy.ts
@@ -0,0 +1,12 @@
+// https://on.cypress.io/api
+
+describe('Header exists', () => {
+  it('visits the app root url', () => {
+    cy.visit('/')
+    // Assert that the Header component exists
+    cy.get('header').should('exist');
+
+    // Assert that the logo exists within the Header component
+    cy.get('header').find('img').should('have.attr', 'src').should('include', 'logo'); // replace 'logo' with the actual name or part of the logo source
+  })
+})
