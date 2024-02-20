e6695e9ad9333011cc4223b0d1489fc19d088221
kasumi993 <khadijag993@gmail.com>
2024-02-20 16:14:23 +0100

    update home link

--- src/components/Headers/Header.vue
+++ src/components/Headers/Header.vue
@@ -3,11 +3,11 @@
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <a
-          href="https://flowbite.com"
+          href="/home"
           class="flex items-center"
         >
           <img
-            alt="Vue logo"
+            alt="Bubble logo"
             class="logo"
             src="@/assets/logo.svg"
             width="25"
