f32b2c487c9832df9d51ecf1e55b07a4c08d2dc4
kasumi993 <khadijag993@gmail.com>
2024-02-23 08:46:41 +0100

    add design (home page and contest page (in progress)
    needs animation for contest page

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
     <a
       href="#"
-      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      class="block max-w-sm p-16 border rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700"
     >
 
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ title }}</h5>
-      <p class="font-normal text-gray-700 dark:text-gray-400">Here is an interesting contest about....</p>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">{{ title }}</h5>
+      <p class="font-normal text-gray-400">Here is an interesting contest about....</p>
     </a>
   </div>
 </template>
